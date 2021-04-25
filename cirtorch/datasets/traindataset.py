d8f8de4d656db389f4e9db67b99104871a2d0343
Ronald Seoh <ronaldseoh@icloud.com>
2021-04-25 19:36:25 -0400

    datasets/traindataset.py: temp

--- cirtorch/datasets/traindataset.py
+++ cirtorch/datasets/traindataset.py
@@ -544,10 +544,9 @@ class TuplesDataset(data.Dataset):
                     nidxs_others = []
 
                     r_others = len(ranks) - 1 # Start from the back
-                    
-                    if not 'clusters' in locals():
-                        qcluster = self.clusters[self.qidxs[q]]
-                        clusters = [qcluster]
+
+                    qcluster = self.clusters[self.qidxs[q]]
+                    clusters = [qcluster]
 
                     while (len(nidxs_others) < self.save_nidxs_others_up_to) and (r_others >= 0):
                         potential = self.idxs2images[ranks[r_others, q]]

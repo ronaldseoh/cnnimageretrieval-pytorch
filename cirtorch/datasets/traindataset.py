74e6288d7dd81a1b5a181d9bea0c3d5e4a5bbb03
Ronald Seoh <ronaldseoh@icloud.com>
2021-04-25 13:59:34 -0400

    datasets/traindataset.py: create `clusters` separately when refresh_nidxs=False and self.store_nidxs_others=True in create_epoch_tuples()

--- cirtorch/datasets/traindataset.py
+++ cirtorch/datasets/traindataset.py
@@ -541,6 +541,10 @@ class TuplesDataset(data.Dataset):
                     nidxs_others = []
 
                     r_others = len(ranks) - 1 # Start from the back
+                    
+                    if not 'clusters' in locals():
+                        qcluster = self.clusters[self.qidxs[q]]
+                        clusters = [qcluster]
 
                     while len(nidxs_others) < self.save_nidxs_others_up_to:
                         potential = self.idxs2images[ranks[r_others, q]]

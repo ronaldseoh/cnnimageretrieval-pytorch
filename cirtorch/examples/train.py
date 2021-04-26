ebc949330e192e3831a523c75e3ce6abdc45d673
Ronald Seoh <ronaldseoh@icloud.com>
2021-04-26 18:21:52 -0400

    examples/train.py: num_worker=0 and pin_memory=False for train and val dataloader

--- cirtorch/examples/train.py
+++ cirtorch/examples/train.py
@@ -330,7 +330,7 @@ def main():
 
     train_loader = torch.utils.data.DataLoader(
         train_dataset, batch_size=args.batch_size, shuffle=True,
-        num_workers=args.workers, pin_memory=True, sampler=None,
+        num_workers=0, pin_memory=False, sampler=None,
         drop_last=True, collate_fn=collate_tuples
     )
     if args.val:
@@ -345,7 +345,7 @@ def main():
         )
         val_loader = torch.utils.data.DataLoader(
             val_dataset, batch_size=args.batch_size, shuffle=False,
-            num_workers=args.workers, pin_memory=True,
+            num_workers=0, pin_memory=False,
             drop_last=True, collate_fn=collate_tuples
         )
 

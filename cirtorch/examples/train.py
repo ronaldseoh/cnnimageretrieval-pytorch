51791b760fc1bbc2b8deeb37654e4c9f983efb6c
Ronald Seoh <ronaldseoh@icloud.com>
2021-04-27 22:54:54 -0400

    examples/train.py: update default args.print_freq value

--- cirtorch/examples/train.py
+++ cirtorch/examples/train.py
@@ -119,8 +119,8 @@ parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
                     help='momentum')
 parser.add_argument('--weight-decay', '--wd', default=1e-6, type=float,
                     metavar='W', help='weight decay (default: 1e-6)')
-parser.add_argument('--print-freq', default=10, type=int,
-                    metavar='N', help='print frequency (default: 10)')
+parser.add_argument('--print-freq', default=1, type=int,
+                    metavar='N', help='print frequency (default: 1)')
 parser.add_argument('--resume', default='', type=str, metavar='FILENAME',
                     help='name of the latest checkpoint (default: None)')
                     

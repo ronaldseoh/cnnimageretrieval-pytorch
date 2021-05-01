42182c2837cd3443424667dc332ed1290cc7e7d1
Ronald Seoh <ronaldseoh@icloud.com>
2021-05-01 14:51:49 -0400

    Revert "examples/train.py: nidxs_others refresh should be disabled by default"
    This reverts commit 195cdf72d03bbc6af6c891c9410c4bc07c9c6fbf.

--- cirtorch/examples/train.py
+++ cirtorch/examples/train.py
@@ -189,11 +189,12 @@ parser.add_argument('--do_not_refresh_query_vectors', action="store_true")
 
 parser.add_argument('--do_not_refresh_nidxs_vectors', action="store_true")
 
-parser.add_argument('--do_not_refresh_nidxs_others_vectors', action="store_false")
+parser.add_argument('--do_not_refresh_nidxs_others_vectors', action="store_true")
 
 parser.add_argument('--do_not_refresh_nidxs', action="store_true")
 
-parser.add_argument('--do_not_refresh_nidxs_others', action="store_false")
+parser.add_argument('--do_not_refresh_nidxs_others', action="store_true")
+
 
 min_loss = float('inf')
 
